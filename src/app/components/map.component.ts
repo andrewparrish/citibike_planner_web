273e13cad9e8ac667d1b74d91d2243ac658a65c0
Andrew Parrish <m.andrewparrish@gmail.com>
2016-11-19 19:55:36 -0500

    Basic menu setup

---
+++ src/app/components/map.component.ts
@@ -0,0 +1,17 @@
+/**
+ * Created by andrewparrish on 11/19/16.
+ */
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.css'],
+  providers: []
+})
+
+export class MapComponent {
+  
+}
+
