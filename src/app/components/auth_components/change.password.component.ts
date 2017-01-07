9a5e89150e6d4db94226288be52c9d1bcbeb43e5
Andrew Parrish <m.andrewparrish@gmail.com>
2017-01-07 18:23:24 -0500

    Adding initial work for changePassword component

--- src/app/components/auth_components/change.password.component.ts
+++ src/app/components/auth_components/change.password.component.ts
@@ -3,24 +3,24 @@ import { Angular2TokenService } from 'angular2-token';
 import { AuthComponent } from "./auth.component";
 
 @Component({
-  selector: 'forgot-password',
-  templateUrl: 'html/forgot.password.component.html',
+  selector: 'change-password',
+  templateUrl: 'html/change.password.component.html',
   styleUrls: ['css/auth.component.css'],
   providers: []
 })
 
-export class ForgotPasswordComponent extends AuthComponent {
-  private email : string;
+export class ChangePasswordComponent extends AuthComponent {
+  private password : string;
 
   constructor(tokenService: Angular2TokenService) {
     super(tokenService);
-    this.email = "";
+    this.password = "";
   }
 
   submit() {
     super.resetError();
     this.tokenService.resetPassword({
-      email: this.email
+      email: this.password
     }).subscribe(
       res => console.log(res),
       error => super.handleError(error)
