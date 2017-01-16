cc8865a96a7585a8112d50101eca005853768309
Andrew Parrish <m.andrewparrish@gmail.com>
2017-01-15 22:47:49 -0500

    Setup for card street view

--- src/app/components/content_components/station.card.component.ts
+++ src/app/components/content_components/station.card.component.ts
@@ -2,7 +2,7 @@ import {Component, Input, EventEmitter, Output} from '@angular/core';
 import { Station } from '../../models/station';
 import { ViewEncapsulation } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
-
+import { Angular2TokenService } from 'angular2-token';
 
 @Component({
     selector: 'station-card',
@@ -16,19 +16,27 @@ export class StationCardComponent{
 	@Input() station: Station;
   @Output() onCloseEmitter: EventEmitter<Boolean>;
   private favorited : boolean;
+  private cardImage : string;
 
-  constructor(private sanitizer: DomSanitizer) {
+  constructor(private sanitizer: DomSanitizer, private tokenService: Angular2TokenService) {
     this.onCloseEmitter = new EventEmitter<Boolean>();
     this.favorited = false;
+    this.cardImage = "https://d21xlh2maitm24.cloudfront.net/nyc/Transparent-Bike.png?mtime=20160420134420";
   }
 
   closeCard() {
     this.onCloseEmitter.emit(true);
   }
 
+  showStreetView() {
+  }
+
+  showMapImage() {
+    this.cardImage = this.station.maps_url;
+  }
+
 
   getImageUrl() {
-    return "https://d21xlh2maitm24.cloudfront.net/nyc/Transparent-Bike.png?mtime=20160420134420";
-    //return this.sanitizer.bypassSecurityTrustResourceUrl(this.station.maps_url);
+    return this.sanitizer.bypassSecurityTrustResourceUrl(this.cardImage);
   }
 }
