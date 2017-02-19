5fd898321cc6c02efa31899d6466569550c82e87
Andrew Parrish <aparrish@jibe.com>
2017-02-18 19:55:23 -0500

    Working leaflet map

--- src/app/components/content_components/map.component.ts
+++ src/app/components/content_components/map.component.ts
@@ -9,31 +9,50 @@ import { Station } from "../../models/station";
 
 import * as L from 'leaflet';
 import { Map } from 'leaflet';
+import {MapsService} from "../../providers/maps.service";
 
 @Component({
   selector: 'map',
   templateUrl: 'html/map.component.html',
   styleUrls: ['css/map.component.css'],
-  providers: [StationApiService]
+  providers: [StationApiService, MapsService]
 })
 
 export class MapComponent extends StationsApiComponent {
   @ViewChild('infopane') infoPane;
 
   private _mapHeight: String;
+  private _mapWidth: String;
   private _currStation: Station;
 
-  protected _map: Map;
+  protected map: Map;
 
-  constructor(stationApiService: StationApiService) {
+  constructor(stationApiService: StationApiService, private mapsService: MapsService) {
     super(stationApiService);
     this._mapHeight = (window.innerHeight - 64).toString() + 'px';
-    this._map = L.map("mapid").setView([40.7128, -74.0059], 12);
+    this._mapWidth = (window.innerWidth) + 'px';
+  }
+
+  ngOnInit() {
+    this.map = L.map("mapid", {
+      zoomControl: false,
+      zoom: 12,
+      center: L.latLng(40.7128, -74.0059),
+      layers: [this.mapsService.baseMaps.OpenStreetMap]
+    });
+
+    L.control.layers(this.mapsService.baseMaps).addTo(this.map);
+    L.control.zoom({ position: "topright" }).addTo(this.map);
   }
 
   mapHeight() {
     return this._mapHeight || '1000px';
   }
+
+  mapWidth() {
+    return this._mapWidth;
+  }
+
   clickedStation(stationId, index): void {
     this._currStation = this.stations[index];
     console.log("INFO PANE", this.infoPane);
---
+++ src/app/providers/maps.service.ts
@@ -0,0 +1,22 @@
+import {Injectable} from "@angular/core";
+import {Http} from "@angular/http";
+
+@Injectable()
+export class MapsService {
+  public baseMaps: any;
+
+  // CREDIT: https://github.com/haoliangyu/angular2-leaflet-starter/blob/master/public_src/services/map.service.ts
+  constructor(private http: Http) {
+    this.baseMaps = {
+      OpenStreetMap: L.tileLayer("http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, Tiles courtesy of <a href="http://hot.openstreetmap.org/" target="_blank">Humanitarian OpenStreetMap Team</a>'
+      }),
+      Esri: L.tileLayer("http://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}", {
+        attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
+      }),
+      CartoDB: L.tileLayer("http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png", {
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
+      })
+    };
+  }
+}
